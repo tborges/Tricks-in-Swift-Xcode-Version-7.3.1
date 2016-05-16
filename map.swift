f9732fbc2e6274b4c0ade5ca616d5f260bf6b0ba
Troy <tborgesmail@yahoo.com>
2016-05-15 23:23:58 -0700

    Using Maps Swift in Xcode Version 7.3.1
    Few lines updated

---
+++ map.swift
@@ -0,0 +1,49 @@
+//
+//  map.swift
+//  GPS Tracker
+//
+//  Created by T.B.B. on 5/10/16.
+//  Copyright © 2016 MrBorges. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class map: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate, UITextFieldDelegate {
+    
+    // Map
+    @IBOutlet weak var mapPage: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // do Nay additional setup after loading the view typically from a nib.
+        
+        let location = CLLocationCoordinate2DMake(48.88182, 2.43952)
+        
+        let span = MKCoordinateSpanMake(0.02, 0.02)
+        
+        let region = MKCoordinateRegionMake(location, span)
+        
+        mapPage.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        //annotation.setCoordinate(location)
+        annotation.coordinate = location
+        annotation.title = "Pizza Pistorante"
+        annotation.subtitle = "Luna Rossa"
+        
+        mapPage.addAnnotation(annotation)
+        
+        //Showing the device location on the map
+        self.mapPage.showsUserLocation = true;
+    
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
+    
+    
+}
